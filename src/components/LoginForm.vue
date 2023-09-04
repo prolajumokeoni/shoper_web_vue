12aefc2caa8099e6f7177a5781242a736c0f0447
prolajumokeoni <prolajumokeoni@gmail.com>
2023-09-04 12:34:30 +0100

    Add login form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,17 @@
+<template>
+	 <div>
+
+<button class="shop">Login to shopper</button>
+<p>Please enter login information</p>
+<form>
+	<label for="email">Email</label>
+	<input type="email" id="email" name="email" placeholder="Email" />
+	<label for="password">Password</label>
+	<input type="password" id="password" name="password" placeholder="Password" />
+	<p>Forgot your password?</p>
+	<button class="submit-btn" type="submit">Login with email</button>
+<p>You do not have an account ? </p>
+<a href="#about">Create an account</a>
+</form>
+</div>
+</template>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,86 +1,70 @@
-<script setup>
-import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
-</script>
+
 
 <template>
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
+  <div class="wrapper">
+    <div class="left">
+      <img src="../assets/frame.png" />
+    </div>
+    <div class="right">
+      <LoginForm />
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </WelcomeItem>
+      <div>
+      <button class="shop">Login to shopper</button>
+      <p>Please enter login information</p>
+      <form>
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" placeholder="Email" />
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" placeholder="Password" />
+        <p>Forgot your password?</p>
+        <button class="submit-btn" type="submit">Login with email</button>
+      <p>You do not have an account ? </p>
+      <a href="#about">Create an account</a>
+      </form>
+    </div>
 
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
-
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank">Cypress Component Testing</a>.
-
-    <br />
-
-    More instructions are available in <code>README.md</code>.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
+    </div>
+  </div>
 </template>
+<style>
+.wrapper {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+}
+.left {
+  width: 50%;
+}
+.right {
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.shop {
+  border: 1px solid black;
+  border-radius: 25px;
+  padding: 10px;
+  color: black;
+  background: white;
+}
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+input {
+  padding: 16px;
+  border-radius: 12px;
+  border: 1px solid #a3a3a3;
+  width: 375px;
+}
+.submit-btn {
+  border-radius: 12px;
+  background: #000;
+  height: 50px;
+  color: white;
+}
+</style>
